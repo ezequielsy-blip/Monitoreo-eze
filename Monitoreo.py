dc6805258bf083b556ee4426d1022a53a892d0d8
ezequielsy-blip <ezequielsy@gmail.com>
2026-02-12 21:20:51 -0300

    Update Monitoreo.py

--- Monitoreo.py
+++ Monitoreo.py
@@ -24,7 +24,7 @@ with st.expander("🔍 CONECTAR CON NETBOOK (LOGISTICA)", expanded=False):
             ip_local = s.getsockname()[0]
             s.close()
             st.success(f"Conectado. Tu IP: {ip_local}")
-            st.info("Buscando Netbook para vincular con APP_STOCK.PY...")
+            st.info("Buscando Netbook para vincular...")
         except:
             st.error("Asegurate de estar en el mismo Wi-Fi que la Netbook.")
 
